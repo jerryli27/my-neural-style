be880ecb990bf9da98489bb9f4594af6b5f0f63b
Jerry Li <jerrylijiaming@gmail.com>
2016-12-18 14:28:59 +0900

    Added skip_noise_4 generator. Testing things out.

--- skip_noise_4_feedforward_net.py
+++ skip_noise_4_feedforward_net.py
@@ -17,33 +17,41 @@ nums_3x3up = [16, 32, 64, 128,128]
 def net(image, mirror_padding=True, reuse=False):
     # TODO: check if we need mirror padding
     prev_layer = image
+    prev_layer_list = [image]
     skip_noise_list = []
+    skip_concat_list = []
 
     with tf.variable_scope('skip_noise_4', reuse=reuse):
         for i in range(len(nums_3x3down)):
+            # deeper first applies a conv with kernel = 3 and stride = 2
+            deeper_1 = _conv_layer(prev_layer, nums_3x3down[i], 3, 2, mirror_padding=mirror_padding, name='deeper_1_%d' %i, reuse=reuse)
+            deeper_2 = _conv_layer(deeper_1, nums_3x3down[i], 3, 1, mirror_padding=mirror_padding, name='deeper_2_%d' %i, reuse=reuse)
+            prev_layer_list.append(deeper_2)
+            prev_layer = deeper_2
+
+        for i in range(len(nums_3x3down)):
+
             # Skip just applies a 1d conv.
-            skip_conv_prev = _conv_layer(prev_layer, nums_1x1[i], 1, 1, relu=False, mirror_padding=mirror_padding, name='skip_conv_prev_%d' %i, reuse=reuse)
-            skip_conv_prev_shape = map(lambda i: i.value, skip_conv_prev.get_shape())
+            skip_conv_prev = _conv_layer(prev_layer_list[i], nums_1x1[i], 1, 1, relu=False, mirror_padding=mirror_padding, name='skip_conv_prev_%d' %i, reuse=reuse)
+            skip_conv_prev_shape = map(lambda s: s.value, skip_conv_prev.get_shape())
             # Then add a noise layer
             skip_noise = tf.placeholder(tf.float32, shape=(skip_conv_prev_shape[0], skip_conv_prev_shape[1], skip_conv_prev_shape[2], nums_noise[i]), name = 'skip_noise_%d' %i)
             skip_noise_list.append(skip_noise)
             skip_concat = tf.concat(3,[skip_conv_prev,skip_noise], name='skip_concat_%d' %i)
-
-            # deeper first applies a conv with kernel = 3 and stride = 2
-            deeper_1 = _conv_layer(prev_layer, nums_3x3down[i], 3, 2, mirror_padding=mirror_padding, name='deeper_1_%d' %i, reuse=reuse)
-            deeper_2 = _conv_layer(deeper_1, nums_3x3down[i], 3, 1, mirror_padding=mirror_padding, name='deeper_2_%d' %i, reuse=reuse)
-
-            skip_deeper_concat = tf.concat(3,[skip_concat,deeper_2], name='skip_deeper_concat_%d' %i)
-            prev_layer = skip_deeper_concat
+            skip_concat_list.append(skip_concat)
 
         # Next is deconv.
+        prev_layer = prev_layer_list[-1]
         for i in range(len(nums_3x3up)):
-            deconv_1 = _conv_layer(prev_layer, nums_3x3up[i], 3, 1, mirror_padding=mirror_padding, name='deconv_1_%d' %i, reuse=reuse)
+            skip_concat_height, skip_concat_width = skip_concat_list[-i - 1].get_shape().as_list()[1:3]
+            upsampled = tf.image.resize_nearest_neighbor(prev_layer, [skip_concat_height, skip_concat_width])
+            deconv_1 = _conv_layer(upsampled, nums_3x3up[i], 3, 1, mirror_padding=mirror_padding, name='deconv_1_%d' %i, reuse=reuse)
             deconv_2 = _conv_layer(deconv_1, nums_3x3up[i], 1, 1, mirror_padding=mirror_padding, name='deconv_2_%d' %i, reuse=reuse)
-            prev_layer = deconv_2
+            skip_deeper_concat = tf.concat(3, [skip_concat_list[-i - 1], deconv_2], name='skip_deeper_concat_%d' % i)
+            prev_layer = skip_deeper_concat
 
-        # Do a final convolution.
-        weights_init = _conv_init_vars(prev_layer, 1, 1, name='final_conv', reuse=reuse)
+        # Do a final convolution with output dimension = 3 and stride 1.
+        weights_init = _conv_init_vars(prev_layer, 3, 1, name='final_conv', reuse=reuse)
         strides_shape = [1, 1, 1, 1]
         final = tf.nn.conv2d(prev_layer, weights_init, strides_shape, padding='SAME')
 
